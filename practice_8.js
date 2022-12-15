588135d9974649b0bba7af307feb943ed1ec962a
ritulsingh <ritulsingh00@gmail.com>
2022-12-15 10:50:25 +0530

    Server using express

---
+++ practice_8.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+const PORT = 3000;
+  
+app.get('/', function(req, res){
+    res.send("Hello World")
+})
+
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
